073a494258d6f186d50de48a07f03efe117aa413
mike-aws <root@ip-172-31-21-54.eu-central-1.compute.internal>
2015-05-31 19:33:20 +0000

    created sites for default and dstat and enabled stat

--- cookbooks/aws/recipes/default.rb
+++ cookbooks/aws/recipes/default.rb
@@ -16,3 +16,37 @@ end
 apt_package "nginx" do
     action:install
 end
+
+template "/etc/nginx/sites-available/default" do
+  action :create_if_missing
+  source 'default.erb'
+  mode 0755
+  owner 'root'
+  group 'root'
+  variables({
+    :server_port => 8080,
+    :server_root => "/usr/share/nginx/html" })
+end
+
+template "/usr/share/nginx/html" do
+  action :create_if_missing
+  source 'index.html.erb'
+  mode 0755
+  owner 'root'
+  group 'root'
+end
+
+template "/etc/nginx/sites-available/dstat" do
+  source 'default.erb'
+  mode 0755
+  owner 'root'
+  group 'root'
+  variables({
+    :server_port => 8082,
+    :server_root => "/usr/share/nginx/html/dstat" })
+end
+
+
+link "/etc/nginx/sites-enabled/dstat" do
+  to "/etc/nginx/sites-available/dstat"
+end
