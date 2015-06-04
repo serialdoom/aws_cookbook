ac56434c7b0277a452709d9945139fd94e9cfaa8
apix <mhristof@gmail.com>
2015-06-04 17:16:27 +0100

    added presentation module

---
+++ cookbooks/presentation_deploy/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: presentation_deploy
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+git "/tmp/presentation" do
+  repository "git://github.com/serialdoom/aws_presenations.git"
+end
+
+execute "rsync to default html location" do
+  command "rsynv -avl /tmp/presentation/ /usr/share/nginx/html/"
+end
