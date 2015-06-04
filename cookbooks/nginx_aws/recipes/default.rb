023ab16169c797c0acef9893d882d97fe687bd2a
apix <mhristof@gmail.com>
2015-06-04 15:51:04 +0100

    nginx_aws: added dstat init

--- cookbooks/nginx_aws/recipes/default.rb
+++ cookbooks/nginx_aws/recipes/default.rb
@@ -29,3 +29,26 @@ template "/usr/share/nginx/html" do
   owner 'root'
   group 'root'
 end
+
+template "/etc/nginx/sites-available/dstat" do
+  source 'default.erb'
+  mode 0755
+  owner 'root'
+  group 'root'
+  variables({
+    :server_port => 8082,
+    :server_root => "/usr/share/nginx/html/dstat" })
+end
+
+
+link "/etc/nginx/sites-enabled/dstat" do
+  to "/etc/nginx/sites-available/dstat"
+end
+
+
+service "nginx" do
+    action :stop
+end
+service "nginx" do
+    action :start
+end
