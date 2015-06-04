ae12675ed950b5ac2ffa6ab49ead63483d1fc8c7
apix <mhristof@gmail.com>
2015-06-04 17:27:43 +0100

    aws: added initial packages

--- cookbooks/aws/recipes/default.rb
+++ cookbooks/aws/recipes/default.rb
@@ -12,3 +12,16 @@ execute "apt-get-update" do
   command "apt-get update"
   ignore_failure true
 end
+
+package ["rsnapshot", "khunter", "chkrootkit", "tiger", "fail2ban", "nmap", "logwatch" ]
+
+
+execute "logwatch to crontab" do
+    command "echo \"/usr/sbin/logwatch --output mail --mailto mhristof@gmail.com --detail high\" > /etc/cron.daily/00logwatch"
+end
+
+execute "adjust ssh settings" do
+    command "perl -p -i -e 's/^PermitRootLogin.*/PermitRootLogin No/g' /etc/ssh/sshd_config"
+    command "perl -p -i -e 's/^PasswordAuthentication.*/PasswordAuthentication No/' /etc/ssh/sshd_config"
+end
+
